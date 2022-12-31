3fdf0e582996390641d6ada84ebe515d97544a15
Stavros Bastakis <staurosmpa@gmail.com>
2022-12-31 09:08:56 +0200

    Create home page hero section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
+import '../styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
