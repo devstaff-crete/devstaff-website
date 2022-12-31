bdc0fd2e9e0aff095c2ecaed306233b537d0cf78
Stavros Bastakis <staurosmpa@gmail.com>
2022-12-31 09:08:56 +0200

    Create skeleton of basic pages

---
+++ pages/events.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+export default function Events() {
+  return (
+    <>
+      <Head>
+        <title>Events</title>
+      </Head>
+      <h1>Events</h1>;
+    </>
+  );
+}
---
+++ pages/sponsors.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Sponsors() {
+  return (
+    <>
+      <Head>
+        <title>Sponsors</title>
+      </Head>
+      <h1>Sponsors</h1>;
+    </>
+  );
+}
