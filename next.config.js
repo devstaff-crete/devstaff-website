f3863d35eb0ea16ec8ea91ab07fc7073f38c1f7c
Stavros Bastakis <staurosmpa@gmail.com>
2023-01-20 01:00:27 +0200

    Install svgr

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
