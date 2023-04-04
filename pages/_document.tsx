56499ebed34ffe160a0a7fa35171c2a2c0c9cb97
Stavros Bastakis <staurosmpa@gmail.com>
2023-04-04 10:38:40 +0300

    Add analytics script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -8,6 +9,18 @@ export default function Document() {
         <link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+               (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+               (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+               m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+               })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+               ga('create', 'UA-64734579-1', 'auto');
+               ga('send', 'pageview');  
+           `}
+        </Script>
       </Head>
       <body>
         <Main />
