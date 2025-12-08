9734bbaf0af7d324825075d65615f44be9a5f460
Iraklis Psaroudakis <kingherc@gmail.com>
2025-12-08 11:54:50 +0200

    Add meetup 104

--- data/events.ts
+++ data/events.ts
@@ -9,6 +9,14 @@ type Event = {
 
 export const events: Event[] = [
   {
+    date: '11/12/2025',
+    time: '19:00',
+    title: '"AI Product Manager" and Other Stupid Job Titles',
+    location: 'FoRTH, Step-C Building',
+    locationUrl: 'https://goo.gl/maps/ZpCiitT3yDCcpumd9',
+    eventUrl: 'https://www.meetup.com/devstaff/events/312335534/'
+  },
+  {
     date: '13/11/2025',
     time: '19:00',
     title: 'Unlocking Smart Home Potential: Automation, Integration, and Innovation',
