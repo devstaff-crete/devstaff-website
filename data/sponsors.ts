fd69ff875d4843553b2458b85906c55c303ef458
Iraklis Psaroudakis <kingherc@gmail.com>
2026-01-21 19:03:29 +0200

    Update domeba sponsor logo

--- data/sponsors.ts
+++ data/sponsors.ts
@@ -23,7 +23,7 @@ import epignosisLogo from '../public/sponsors/epignosis-logo.png';
 import netcompanyLogo from '../public/sponsors/netcompany.png';
 import tourmieLogo from '../public/sponsors/Tourmie-logo.png';
 import otsLogo from '../public/sponsors/OTS_logo_black.png';
-import domebaLogo from '../public/sponsors/logo-domeba-white.jpeg';
+import domebaLogo from '../public/sponsors/domeba.png';
 import zarosLogo from '../public/sponsors/zaros.png';
 import cytechLogo from '../public/sponsors/cytech-logo.png';
 
