004d79f012a0e0bef6c02956abda5022f0ed909c
Mihalis Zampetakis <mzampetakis@gmail.com>
2026-07-09 12:24:03 +0300

    remove duplicate

--- data/events.ts
+++ data/events.ts
@@ -25,14 +25,6 @@ export const events: Event[] = [
     eventUrl: 'https://www.meetup.com/devstaff/events/314991412/'
   },
   {
-    date: '04/06/2026',
-    time: '19:00',
-    title: "Investments for Geeks",
-    location: 'FoRTH, Step-C Building',
-    locationUrl: 'https://goo.gl/maps/ZpCiitT3yDCcpumd9',
-    eventUrl: 'https://www.meetup.com/devstaff/events/314693234/'
-  },
-  {
     date: '14/05/2026',
     time: '19:00',
     title: "Investments for Geeks",
