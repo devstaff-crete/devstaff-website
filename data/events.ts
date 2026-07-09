3416373682f8e85e4f0fe4f467a20c3a105c13b2
Mihalis Zampetakis <mzampetakis@gmail.com>
2026-07-09 12:48:58 +0300

    fix dates

--- data/events.ts
+++ data/events.ts
@@ -9,7 +9,7 @@ type Event = {
 
 export const events: Event[] = [
   {
-    date: '14/05/2026',
+    date: '07/07/2026',
     time: '19:00',
     title: "Guiding Engineering Teams with AI",
     location: 'FoRTH, Step-C Building',
@@ -17,7 +17,7 @@ export const events: Event[] = [
     eventUrl: 'https://www.meetup.com/devstaff/events/315513609/'
   },
   {
-    date: '14/05/2026',
+    date: '04/06/2026',
     time: '19:00',
     title: "Building Resilient Global Data & Presence Platforms",
     location: 'FoRTH, Step-C Building',
